d357d5ff7d1073d6ba6d92743fcbb54661871aca
MBP 2017 <38024526+NitramDivad@users.noreply.github.com>
2018-10-08 17:31:21 -0600

    Updates

---
+++ precache-manifest.0c4efbcfdacb6369a256334b5ea040f0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "59843ae1c2ed89599fd0",
+    "url": "/ClickyGame/static/js/runtime~main.59843ae1.js"
+  },
+  {
+    "revision": "0510399fce64fb4c8225",
+    "url": "/ClickyGame/static/js/main.0510399f.chunk.js"
+  },
+  {
+    "revision": "709840df12e4494d4644",
+    "url": "/ClickyGame/static/js/1.709840df.chunk.js"
+  },
+  {
+    "revision": "0510399fce64fb4c8225",
+    "url": "/ClickyGame/static/css/main.0bb1e487.chunk.css"
+  },
+  {
+    "revision": "6f7fc035a2869e6e5a8e19b220088a65",
+    "url": "/ClickyGame/index.html"
+  }
+];
